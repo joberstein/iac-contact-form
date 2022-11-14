44324e84ae22a5834b19280fa93cb4a79fb8284f
Jesse Oberstein <7157500+joberstein@users.noreply.github.com>
2022-11-14 01:38:24 -0500

    fix(send-email): Add source code hash to trigger lambda function updates.
    Closes #38

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,18 +1,24 @@
 locals {
-  build_path    = "${path.module}/sendEmail/target"
   artifact_name = "sendEmail"
+  filename = join("/", [
+    path.module,
+    local.artifact_name,
+    "target",
+    "${local.artifact_name}-jar-with-dependencies.jar"
+  ])
 }
 
 resource "aws_lambda_function" "send_email_lambda" {
-  function_name = "${local.artifact_name}-${var.environment}"
-  filename      = "${local.build_path}/${local.artifact_name}-jar-with-dependencies.jar"
-  description   = "Sends an email out with SimpleEmailService."
-  runtime       = "java11"
-  handler       = "joberstein.portfolio.SendEmailHandler"
-  role          = var.send_email_role_arn
-  timeout       = 15
-  memory_size   = 512
-  publish       = true
+  function_name    = "${local.artifact_name}-${var.environment}"
+  filename         = local.filename
+  source_code_hash = filebase64sha256(local.filename)
+  description      = "Sends an email out with SimpleEmailService."
+  runtime          = "java11"
+  handler          = "joberstein.portfolio.SendEmailHandler"
+  role             = var.send_email_role_arn
+  timeout          = 15
+  memory_size      = 512
+  publish          = true
 
   environment {
     variables = {
